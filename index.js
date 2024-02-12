b1d4f4e720c002806794a611545865de5659e3fd
Gwenael Girard <gwenael@girard-pro.ch>
2024-02-12 09:15:28 +0100

    Amélioration du message de bienvenue

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-#! /usr/bin/env node
-
-// Importing the "program" object from the "commander" library
 import { program } from "commander";
-
-// Importing commands defined in respective files
 import { listProjectsCommand } from "./commands/list.js";
 import { openProjectsCommand } from "./commands/open.js";
 import { configurePathCommand } from "./commands/config.js";
+
+import figlet from "figlet";
+import chalkAnimation from "chalk-animation";
 
 // Adding commands to the "program" object
 program.addCommand(listProjectsCommand());
 program.addCommand(openProjectsCommand());
 program.addCommand(configurePathCommand());
+
+// Setting a global description for your program
+program.description(
+    figlet.textSync("GitOpener", {
+        font: "Big",
+        whitespaceBreak: false,
+    }) + "\nA tool for efficiently opening your Git projects.\n-------------------------------------------------"
+);
 
 // Parsing command line arguments
 program.parse();
